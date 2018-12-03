49baf3a3a5bfab945773ea825f1f2e0bfacba2fd
Rafael Alfaro <rafael.alfaro@bvmedia.cr>
2018-12-03 16:28:51 -0600

    Adds sourcemap to production build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { cube } from './math.js';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
+} else {
+  console.log('Looks like we are in production mode!');
 }
 
 function component() {
